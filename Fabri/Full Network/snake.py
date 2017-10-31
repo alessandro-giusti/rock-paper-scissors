1fe3f1542591ad290d1523393fd3d5b35da1d772
papero2 <fpatuzzo@bluewin.ch>
2017-10-31 09:00:53 +0100

    snake
    punultimate layer

--- Fabri/Full Network/snake.py
+++ Fabri/Full Network/snake.py
@@ -7,12 +7,12 @@ import keras
 from keras import backend as K
 
 # USER OPTIONS
-model_name="nemobest.model"
-class1="IMAGES/nemo.jpg"
-class2="IMAGES/dory.jpg"
+model_name="rps.model"
+class1="IMAGES/rock.jpg"
+class2="IMAGES/paper.jpg"
 class3="IMAGES/scissors.jpg"
-legend=skimage.io.imread("IMAGES/nemo_legend.bmp")
-feed="color"
+legend=skimage.io.imread("IMAGES/rps_legend.bmp")
+
 # VARIABLES
 model = keras.models.load_model(model_name)
 nc=model.output_shape[1]
@@ -26,7 +26,6 @@ output.append(skimage.io.imread(class2))
 output.append(skimage.io.imread(class3))
 get = K.function([model.layers[0].input], [model.layers[13].output])
 get2=[]*11
-Z=350
 b2, F, G, H=0, 0, 0, 0
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
@@ -37,7 +36,7 @@ def adapt_input(im, size):
     im=im[(h//2-sz//2):(h//2+sz//2),(w//2-sz//2):(w//2+sz//2),:] 
     im = skimage.transform.resize(im, (size, size, 3), mode='reflect')
     return im
-a_input="IMAGES/cgreen.jpg"
+a_input="IMAGES/cred.jpg"
 input2=skimage.img_as_ubyte(adapt_input(skimage.io.imread(a_input), 64))
 input2b=skimage.img_as_ubyte(adapt_input(cv2.cvtColor(skimage.io.imread(a_input), cv2.COLOR_BGR2RGB), 80))
 
@@ -45,6 +44,9 @@ input2b=skimage.img_as_ubyte(adapt_input(cv2.cvtColor(skimage.io.imread(a_input)
 cap = cv2.VideoCapture(0)
 ret2, image2 = cap.read()
 
+Z=image2.shape[1]//2
+center=image2.shape[0]*2//3-40
+length=150
 for i in range(11):
     get2.append(K.function([model.layers[0].input], [model.layers[i].output]))
 frame2 = []
@@ -59,53 +61,30 @@ image2[:, :, 0:3].fill(255)
 # TITLE
 image2[8:8+title.shape[0],60:60+title.shape[1],0:3]=title
 # LABEL1
-image2[image2.shape[0]-40:image2.shape[0]-24,10:270,0:3]=x_axis
+image2[image2.shape[0]//3:image2.shape[0]//3+16,10:270,0:3]=x_axis
 # LABEL2
-image2[image2.shape[0]-20:image2.shape[0]-6,111:173,0:3]=neuron_nr
+image2[image2.shape[0]//3+16:image2.shape[0]//3+30,111:173,0:3]=neuron_nr
 # LEGEND
 W=240
-image2[image2.shape[0]-30-legend.shape[0]:image2.shape[0]-30,image2.shape[1]-W-legend.shape[1]:image2.shape[1]-W,0:3]=cv2.cvtColor(legend, cv2.COLOR_BGR2RGB)
-# INPUT
-for j in range(3):
-    A2 = 200+(64*3+2)//2-64-(64+1)*j
-    image2[A2:A2+64, 10:74, 0:3].fill(0)
-        
-B=94
-for j in range(11):
-    C=images3[j].shape[2]
-    D=images3[j].shape[0]
-    for i in range(images3[j].shape[2]):
-        A = 200+(D*C+C-1)//2-D-(D+1)*i
-        image2[A:A+D, B:B+D, 0:3].fill(0)
-    B=B+10+D
+image2[40:40+legend.shape[0],40:40+legend.shape[1],0:3]=cv2.cvtColor(legend, cv2.COLOR_BGR2RGB)
+# GRAPH2
+cv2.line(image2, (Z, center), (Z, center-length), (0,0,255), 2)
+cv2.line(image2, (Z, center), (Z+length, center+length//2), (255,0,0), 2)
+cv2.line(image2, (Z, center), (Z-length, center+length//2), (0,127,0), 2)
 
-##def colorMatrix(layer):
-##    A = np.zeros((3, 5))    
-##    for l in range(5):
-##        for k in range (3):
-##            for i in range (3):
-##                for j in range (3):
-##                    A[k][l] += model.layers[layer].get_weights()[0][i][j][k][l]
-###                    if l==2:
-###                        if k==2:
-###                            print (model.layers[layer].get_weights()[0][i][j][k][l])
-###                            if i==2:
-###                                if j==2:
-###                                    print (A[k][l])
-##    return A
-##A1 = np.round(colorMatrix(0), decimals=1)
-##print ("\nColor convolution 1 \n", A1)
+#    cv2.rectangle(image,(image.shape[1]-150,10),image.shape[1]-50,100,(0,255,0),3)
+
 
 ## MAIN LOOP
 while(True):
     ret, image = cap.read()
     size=200
     A, B, C, D = int(image.shape[0]//2-size/2)-40, int(image.shape[0]//2+size/2)-40, int(image.shape[1]//2-size/2), int(image.shape[1]//2+size/2)
+    Z=image2.shape[1]//2
+    center=image2.shape[0]*2//3-40
     frame = []
-    if feed=="video":
-        frame.append(adapt_input(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), 64))
-    else:
-        frame.append(input2)
+    frame.append(adapt_input(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), 64))
+#    frame.append(input2)
     a = model.predict(np.asarray(frame)).tolist()
     b = a[0].index(max(a[0]))
     f_v = get([frame])[0][0]
@@ -118,39 +97,41 @@ while(True):
     image[:, :, :]=image2[:,:,:]
                
 # INPUT
-    if feed=="video":
-        image[50:130, image.shape[1]-130:image.shape[1]-50]=input_image                
-    else:
-        image[50:130, image.shape[1]-130:image.shape[1]-50]=input2b                
-
-# LAYERS
-    for j in range(3):
-        A2 = 200-(64*3+2)//2+(64+1)*j    
-    if feed=="video":
-        image[A2:A2+64, 10:74, (2-j)%3]=input_image2[:,:,j]
-    else:
-        image[A2:A2+64, 10:74, (2-j)%3]=input2[:,:,j]
-
-    B=94
-    for j in range(11):
-        C=images[j].shape[2]
-        D=images[j].shape[0]
-        for i in range(C):
-            A = 200-(D*C+C-1)//2+(D+1)*i
-            if b2!=b:
-                image[A:A+D, B:B+D, 0:3].fill(0)
-            image[A:A+D, B:B+D, (2+b)%3]=images[j][:, :, i]
-        B=B+10+D
+    image[image.shape[0]-120:image.shape[0]-40, 40:120]=input_image                
+#    image[50:130, image.shape[1]-130:image.shape[1]-50]=input2b                
         
 # PENULTIMATE LAYER
     for i in range(128):
         for j in range (nc):
             w[j][i]=model.layers[15].get_weights()[0][i][j]
         color=np.argmax(np.asarray(w)[:,i])
-        F, G, H=image.shape[0]-41-int(f_v[i]*50), image.shape[0]-40, 14+i*2
+        F, G, H=image.shape[0]//3-int(f_v[i]*50), image.shape[0]//3-1, 14+i*2
         image[F:G,H:H+2,0:3].fill(0)
         image[F:G,H:H+2,(color-1)%3].fill(255)
 
+# GRAPH        
+##        if f_v[i]>0.2 and nc>2:
+##            y = int(center-w[0][i]*length+length//2*w[1][i]+length//2*w[2][i])
+##            x = int(Z+10*(8.66*w[1][i]-8.66*w[2][i]))
+##            if color==0:
+##                cv2.circle(image,(x, y), 3, (0,0,255), -1)
+##            elif color==1:
+##                cv2.circle(image,(x, y), 3, (255,0,0), -1)
+##            else:
+##                cv2.circle(image,(x, y), 3, (0,127,0), -1)
+        
+        y = int(-w[0][i]*length+length//2*w[1][i]+length//2*w[2][i])
+        x = int(10*(8.66*w[1][i]-8.66*w[2][i]))
+        cv2.line(image, (Z, center), ((Z+int(x*f_v[i]/3)), center+int(y*f_v[i]/3)), (0,0,255), 2)
+ #       Z=Z+int(x*f_v[i]/3)
+ #       center=center+int(y*f_v[i]/3)
+##        if color==0:
+##                cv2.circle(image,(x, y), 3, (0,0,255), -1)
+##            elif color==1:
+##                cv2.circle(image,(x, y), 3, (255,0,0), -1)
+##            else:
+##                cv2.circle(image,(x, y), 3, (0,127,0), -1)
+    
 # OUTPUT
     image[image.shape[0]-130:image.shape[0]-50, image.shape[1]-130:image.shape[1]-50]=output[b]
     
@@ -163,26 +144,10 @@ while(True):
 cap.release()
 cv2.destroyAllWindows()
 
-# GRAPH2
-#if nc==3:
-#        cv2.line(image2, (Z, image2.shape[0]-60), (Z, image2.shape[0]-110), (0,0,255), 2)
-#        cv2.line(image2, (Z, image2.shape[0]-60), (Z+50, image2.shape[0]-35), (255,0,0), 2)
-#        cv2.line(image2, (Z, image2.shape[0]-60), (Z-50, image2.shape[0]-35), (0,127,0), 2)
-#    cv2.rectangle(image,(image.shape[1]-150,10),image.shape[1]-50,100,(0,255,0),3)
 ### BORDO
 ###    image[50:150,image.shape[1]-150:image.shape[1]-149,0:3]=np.asarray([[[10]*3 for i in range(1)]for j in range(100)])
 ###    image[A-2:A,0:image.shape[1],0:3]=np.asarray([[[10]*3 for i in range(image.shape[1])]for j in range(2)])
 ###    image[A:B,D-1:D,0:3]=np.asarray([[[10]*3 for i in range(1)]for j in range(B-A)])
 ###    image[B-2:B,0:image.shape[1],0:3]=np.asarray([[[10]*3 for i in range(image.shape[1])]for j in range(2)])
-# GRAPH        
-##        if f_v[i]>0.2 and nc>2:
-##            y = int(image.shape[0]-60-w[0][i]*50+25*w[1][i]+25*w[2][i])
-##            x = int(Z+5*(8.66*w[1][i]-8.66*w[2][i]))
-##            if color==0:
-##                cv2.circle(image,(x, y), 3, (0,0,255), -1)
-##            elif color==1:
-##                cv2.circle(image,(x, y), 3, (255,0,0), -1)
-##            else:
-##                cv2.circle(image,(x, y), 3, (0,127,0), -1)
-# OUTPUT
+
 
